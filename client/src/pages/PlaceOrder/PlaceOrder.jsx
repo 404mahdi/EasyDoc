101df35f167e56ba89877da64ba8dd456bbe4317
Faisal <faisalislam469@gmail.com>
2025-05-16 22:05:13 +0600

    Pharma rehaul

---
+++ client/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,110 @@
+import React, { useContext, useEffect, useState } from "react";
+import "./PlaceOrder.css";
+import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useNavigate } from 'react-router-dom';
+import Navbar from "../../components/Navbar/Navbar";
+
+const PlaceOrder = () => {
+  const navigate = useNavigate();
+
+  const { getTotalCartAmount, drug_list, cartItems, url } =
+    useContext(StoreContext);
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: "",
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  const placeOrder = async (event) => {
+    event.preventDefault();
+    let orderItems = [];
+    drug_list.map((item) => {
+      if (cartItems[item._id] > 0) {
+        let itemInfo = item;
+        itemInfo["quantity"] = cartItems[item._id];
+        orderItems.push(itemInfo);
+      }
+    });
+    let orderData = {
+      address: data,
+      items: orderItems,
+      amount: getTotalCartAmount() + 2,
+    };
+
+    try {
+      let response = await axios.post(url + "/api/order/place", orderData);
+      if (response.data.success) {
+        toast.success("Order placed successfully!");
+        navigate("/myorders");
+      } else {
+        toast.error("Error placing order!");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Error placing order!");
+    }
+  };
+
+
+
+  return (
+    <>>
+    <Navbar/>
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <input type="email" placeholder="Email" />
+        <input type="text" placeholder="Street" />
+        <div className="multi-fields">
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="State" />
+        </div>
+        <input type="text" placeholder="Phone" />
+        </div>
+        <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotals</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+            </div>
+          </div>
+          <button>PROCEED TO Payment</button>
+        </div>
+        </div>
+      
+    </form>
+    </>
+  );
+};
+
+export default PlaceOrder;
